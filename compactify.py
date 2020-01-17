0c5fc180051565206da41d914ea8423085136885
Brandon Sachtleben <5560621+BrandonRiemann@users.noreply.github.com>
2020-01-17 07:55:47 -0500

    Organize code into class, methods

--- compactify.py
+++ compactify.py
@@ -59,124 +59,133 @@ def A_weight(fs):
 def plot_signal(signal, fs):
     plt.plot(np.linspace(0, len(signal)/fs, len(signal)), signal)
 
-# Load the audio file
-audio = AudioSegment.from_file(sys.argv[1])
+class Audio:
+    def __init__(self, filename):
+        # Load the audio file
+        self.audio = AudioSegment.from_file(filename)
 
-# Get the sample rate and numpy array of the sound data
-fs = audio.frame_rate
-types = [np.uint8, np.int16, np.int32, np.int32]
-data = np.fromstring(audio._data, types[audio.sample_width - 1])
-temp = []
-for ch in list(range(audio.channels)):
-    temp.append(data[ch::audio.channels])
-x = np.array(temp).T
-x = x.flatten()
+        # Get the sample rate and numpy array of the sound data
+        self.fs = self.audio.frame_rate
+        self.types = [np.uint8, np.int16, np.int32, np.int32]
+        x = np.fromstring(self.audio._data, self.types[self.audio.sample_width - 1])
+        temp = []
+        for ch in list(range(self.audio.channels)):
+            temp.append(x[ch::self.audio.channels])
+        self.data = np.array(temp).T
+        self.data = self.data.flatten()
 
-# Parameters
-window_length = 100
-# TODO: Need a way to detect a good value for threshold (will depend on the audio and sample width)
-threshold = int(sys.argv[2]) if len(sys.argv) == 3 else 10000000
+        # Parameters
+        self.window_length = 100
+        self.threshold = int(sys.argv[2]) if len(sys.argv) == 3 else 10000000
 
-pbar_step = np.floor(len(x) / 20)
-pbar_total = len(x) + 4*pbar_step
+    def remove_silence(self, plot = False):
+        # Progress bar
+        pbar_step = len(self.data)
+        pbar_total = 5*pbar_step
+        pbar = tqdm(total = pbar_total)
 
-# Progress bar
-pbar = tqdm(total = pbar_total)
+        # Plot 1 - unmodified original audio
+        plt.subplot(3, 1, 1)
+        plot_signal(self.data, self.fs)
+        plt.title("Original audio")
 
-# Plot 1 - unmodified original audio
-plt.subplot(3, 1, 1)
-plot_signal(x, fs)
-plt.title("Original audio")
+        # Apply A-weighting first
+        b, a = A_weight(self.fs)
+        y = filtfilt(b, a, self.data)
 
-# Apply A-weighting first
-b, a = A_weight(fs)
-y = filtfilt(b, a, x)
+        pbar.update(pbar_step)
 
-pbar.update(pbar_step)
+        # Plot 2 - A-weighting applied to samples
+        plt.subplot(3, 1, 2)
+        plot_signal(y, self.fs)
+        plt.title("A-weighted")
 
-# Plot 2 - A-weighting applied to samples
-plt.subplot(3, 1, 2)
-plot_signal(y, fs)
-plt.title("A-weighted")
+        # Get an envelope
+        analytic_signal = hilbert(y)
+        y_env = np.abs(analytic_signal)
 
-# Get an envelope
-analytic_signal = hilbert(y)
-y_env = np.abs(analytic_signal)
+        pbar.update(pbar_step)
 
-pbar.update(pbar_step)
+        # Plot 3 - envelope
+        plt.subplot(3, 1, 3)
+        plot_signal(y_env, self.fs)
+        plt.title("Envelope")
 
-# Plot 3 - envelope
-plt.subplot(3, 1, 3)
-plot_signal(y_env, fs)
-plt.title("Envelope")
-# Uncomment below if you wish to see the plots
-#plt.show()
-plt.savefig("{0:s}_processed.png".format(sys.argv[1][0:-4]))
-plt.close()
+        if plot:
+            plt.show()
 
-segments = []
+        plt.savefig("{0:s}_processed.png".format(sys.argv[1][0:-4]))
+        plt.close()
 
-# Get non-silent segments
-for i in range(0, len(y_env), window_length):
-    Y = y_env[i:i+window_length+1]
+        segments = []
 
-    mean = Y.mean()*(1 + int((i-window_length) in segments)*0.5)
-    if mean > threshold:
-        segments.append(i)
+        # Get non-silent segments
+        for i in range(0, len(y_env), self.window_length):
+            Y = y_env[i:i+self.window_length+1]
 
-    pbar.update(window_length * int(i > 0))
+            mean = Y.mean()*(1 + int((i-self.window_length) in segments)*0.5)
+            if mean > self.threshold:
+                segments.append(i)
 
-pbar.update(len(y_env) - window_length * np.floor(len(y_env)/window_length))
+            pbar.update(self.window_length * int(i > 0))
 
-# Plot for showing regions detected that have audio above threshold value
-fig, ax = plt.subplots()
-fig.set_size_inches(15, 6, forward=True)
-ax.plot(np.linspace(0, len(x)/fs, len(x)), x)
+        pbar.update(len(y_env) - self.window_length * np.floor(len(y_env)/self.window_length))
 
-start_seg = segments[0]
-is_start_seg = True
+        # Plot for showing regions detected that have audio above threshold value
+        fig, ax = plt.subplots()
+        fig.set_size_inches(15, 6, forward=True)
+        ax.plot(np.linspace(0, len(self.data)/self.fs, len(self.data)), self.data)
 
-# Plot regions of audio above threshold (There is certainly a more elegant way to do this.)
-for i in range(0, len(segments)):
-    if (i < len(segments)-1):
-        # marks the end of a segment
-        if is_start_seg and (segments[i+1]/fs-segments[i]/fs) > 0.13:
-            plt.axvspan(start_seg/fs, segments[i]/fs, facecolor='g', alpha=0.5)
-            is_start_seg = False
-        # marks the start of a segment
-        elif not is_start_seg and (segments[i+1]/fs-segments[i]/fs) <= 0.1:
-            start_seg = segments[i]
-            is_start_seg = True
-    else:
-        if is_start_seg:
-            plt.axvspan(start_seg/fs, segments[i]/fs, facecolor='g', alpha=0.5)
-            is_start_seg = False
+        start_seg = segments[0]
+        is_start_seg = True
 
-pbar.update(pbar_step)
+        # Plot regions of audio above threshold (There is certainly a more elegant way to do this.)
+        for i in range(0, len(segments)):
+            if (i < len(segments)-1):
+                # marks the end of a segment
+                if is_start_seg and (segments[i+1]/self.fs-segments[i]/self.fs) > 0.13:
+                    plt.axvspan(start_seg/self.fs, segments[i]/self.fs, facecolor='g', alpha=0.5)
+                    is_start_seg = False
+                # marks the start of a segment
+                elif not is_start_seg and (segments[i+1]/self.fs-segments[i]/self.fs) <= 0.1:
+                    start_seg = segments[i]
+                    is_start_seg = True
+            else:
+                if is_start_seg:
+                    plt.axvspan(start_seg/self.fs, segments[i]/self.fs, facecolor='g', alpha=0.5)
+                    is_start_seg = False
 
-plt.title("Detected silence")
-# Uncomment below to show the plot
-#plt.show()
-plt.savefig("{0:s}_segments.png".format(sys.argv[1][0:-4]))
-plt.close()
+        pbar.update(pbar_step)
 
-# Splice data and write out wav file
+        plt.title("Detected silence")
+
+        if plot:
+            plt.show()
+
+        plt.savefig("{0:s}_segments.png".format(sys.argv[1][0:-4]))
+        plt.close()
+
+        # Splice data segments
+        out = np.array([], dtype=self.types[self.audio.sample_width - 1])
+
+        for i in segments:
+            out = np.append(out, self.data[i:i+self.window_length])
+
+        # Final plot showing truncated output
+        plot_signal(out, self.fs)
+        plt.title("Truncated audio")
+        plt.savefig("{0:s}_trunc.png".format(sys.argv[1][0:-4]))
+
+        pbar.update(pbar_total - pbar.n)
+        pbar.close()
+
+        return out
+
+    def export(self, filename, data):
+        self.audio._data = data
+        self.audio.export(filename, format='wav')
+
+audio = Audio(sys.argv[1])
+data = audio.remove_silence(plot = True)
 strOut = "{0:s}_cut.wav".format(sys.argv[1][0:-4])
-out = np.array([], dtype=types[audio.sample_width - 1])
-
-for i in segments:
-    out = np.append(out, x[i:i+window_length])
-
-# Final plot showing truncated output
-plot_signal(out, fs)
-plt.title("Truncated audio")
-plt.savefig("{0:s}_trunc.png".format(sys.argv[1][0:-4]))
-
-audio._data = out
-# Uncomment below if you wish to also speed up the playback or see pydub docs for more effects
-#audio = AudioSegment.speedup(audio, playback_speed=2)
-
-audio.export(strOut, format='wav')
-
-pbar.update(pbar_total - pbar.n)
-pbar.close()
+audio.export(strOut, data)
